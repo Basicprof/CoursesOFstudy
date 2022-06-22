04d1988515eafd482046adc487abe5786aa7eca7
StudyUser <basicprof@gmail.com>
2022-06-22 06:39:17 +0300

    settings.py

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -70,8 +70,12 @@ WSGI_APPLICATION = 'educa.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE':'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE':'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'rootroot',
     }
 }
 # Password validation
