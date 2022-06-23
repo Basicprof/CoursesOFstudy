5d9f4ce420add3760f650377cd8e21b8a0a2e627
StudyUser <basicprof@gmail.com>
2022-06-23 08:31:35 +0300

    add postgres worker

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -70,12 +70,14 @@ WSGI_APPLICATION = 'educa.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        # 'ENGINE':'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #   'ENGINE':'django.db.backends.sqlite3',
+        #   'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'rootroot',
+        'HOST': '127.0.0.1', 
+        'PORT': '5433'
     }
 }
 # Password validation
