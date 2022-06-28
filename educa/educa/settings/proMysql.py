01377485aeb692e7f08e9b2b4a517985b2e65695
StudyUser <basicprof@gmail.com>
2022-06-28 19:29:46 +0300

    add Mysql

---
+++ educa/educa/settings/proMysql.py
@@ -0,0 +1,20 @@
+from .base import *
+DEBUG = True
+ADMINS = (
+     ('Antonio M', 'basicprof@gmail.com'),
+)
+# ALLOWED_HOSTS = ['educaproject.com', 'www.educaproject.com']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'coursesall$coursesdb',
+        'USER': 'coursesall',
+        'PASSWORD': 'CreateDb',
+        'HOST': 'coursesall.mysql.pythonanywhere-services.com',
+        'TEST': {
+          'NAME': '<coursesall>$test_<coursesdb>',
+          }
+    }
+}
+ALLOWED_HOSTS = ['coursesall.pythonanywhere.com',]
+ 
